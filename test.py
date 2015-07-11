5fda94fdfa2a39bc6ea44691cc0d60e03d9270da
Juls <jtan2284@gmail.com>
2015-07-11 14:16:02 +1000

    My Django Girls app, first commit

---
+++ test.py
@@ -0,0 +1,20 @@
+def hi(name):
+	print('Hi ' + name +'!')
+hi("Juls")
+
+girls = ['Jenny', 'Steph', 'Amy', 'You']
+for name in girls:
+	hi(name)
+	print('Next girl')
+for i in range(1, 4):
+	print(i)
+
+	mysite/settings.py.
+	    doc = "py property."
+	    def fget(self):
+	        return sepy
+	    def fset(self, value):
+	        sepy = value
+	    def fdel(self):
+	        del sepy
+	    return localspy = propertpy())
